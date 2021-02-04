ea0d9a43da53e8563868e86d4b8a14ee6dcca1c2
kuzminal <kuzmin.al35@gmail.com>
2021-02-04 17:47:50 +0300

    working with signed JWT by getting public key from auth server

--- src/main/java/com/kuzmin/security/resourceserver/config/ResourceServerConfig.java
+++ src/main/java/com/kuzmin/security/resourceserver/config/ResourceServerConfig.java
@@ -2,8 +2,9 @@ package com.kuzmin.security.resourceserver.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
 
 @Configuration
 @EnableResourceServer
-public class ResourceServerConfig {
+public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
 }
